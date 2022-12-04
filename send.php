b13b713b6459533e580fb70a5027e2b30e811c90
ilyanishtun <agent1-007@mail.ru>
2022-12-05 00:38:59 +0600

    check

--- send.php
+++ send.php
@@ -1,4 +1,4 @@
-<meta http-equiv='refresh' content='5; url=http://sarybai.lh1.in'>
+<meta http-equiv='refresh' content='3; url=http://sarybai.lh1.in'>
 <meta charset="UTF-8" />
 
 <?php
@@ -14,6 +14,8 @@ if ($_POST['act'] == 'order') {
     $email = ($_POST['email']);
     //$phone = ($_POST['phone']);
 
+
+
 //Собираем в массив то, что будет передаваться боту
     $arr = array(
         'E-mail:' => $email,
@@ -26,19 +28,62 @@ if ($_POST['act'] == 'order') {
     };
 
 //Передаем данные боту
-    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+}
+
+else{
+?>
+
+    <script language="javascript" type="text/javascript">
+    alert('Нас не проведешь - это не e-mail. Введите e-mail!');
+    window.location = "http://sarybai.lh1.in";
+    </script>
+  
+<?php
+	
+    } 
+
+    
 
 //Выводим сообщение об успешной отправке
-    if ($sendToTelegram) {
+
+
+ if ($sendToTelegram) {
         //alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
-        echo "Спасибо! Мы записали Ваш e-mail.";
+        //echo "<center><h1>Спасибо! Мы записали Ваш e-mail.</h1></center>";
+      	
+
+?>
+  
+  <script language="javascript" type="text/javascript">
+    alert('Спасибо! Мы записали Ваш e-mail.');
+    window.location = "http://sarybai.lh1.in";
+  </script>
+  
+<?php
     }
 
 //А здесь сообщение об ошибке при отправке
     else {
        // alert('Что-то пошло не так. Попробуйте отправить форму ещё раз.');
-       echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
+       //echo "Ошибка, сообщение не отправлено! Возможно, проблемы на сервере";
+
+?>
+
+    <script language="javascript" type="text/javascript">
+    alert('Упс, что-то пошло не так. Возможно проблемы на сервере.');
+    window.location = "http://sarybai.lh1.in";
+    </script>
+  
+<?php
+	
     }
 }
 
 ?>
+
+
+
+
